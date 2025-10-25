d99ac69e23606686c16287a88b94b2782fdeaf55
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-10-25 15:06:18 +0200

    ticks for x axis OK

--- graphics/utils.py
+++ graphics/utils.py
@@ -77,27 +77,28 @@ def plot_3d_axis(ax, origin, length, direction_id,
             
         ax.plot(axis_vector[0], axis_vector[1], axis_vector[2], color=color, linewidth=line_width, zorder=z_order)
 
-        '''        
+              
         for tick in tick_list:
                 
             # plot the tick line
             ax.plot(
                 [scale(tick[0], origin[0], scale_factor[0])] * 2,
                 [
-                    scale((origin[1] + length[1] * axis_origin[1]), origin[1], scale_factor[1]), scale((origin[1] + length[1] * axis_origin[1]) + tick_length[0] * length[1], (origin[1] + length[1] * axis_origin[1]), scale_factor[1])
+                    scale((origin[1] + length[1] * axis_origin[0][0]), origin[1], scale_factor[1]), 
+                    scale((origin[1] + length[1] * axis_origin[0][0]) + tick_length[0] * length[1], (origin[1] + length[1] * axis_origin[0][0]), scale_factor[1])
                 ],
-                [scale((origin[2] + length[2] * axis_origin[2]), origin[2], scale_factor[2])] * 2,
+                [scale((origin[2] + length[2] * axis_origin[0][1]), origin[2], scale_factor[2])] * 2,
                 color=color, linewidth=line_width, zorder=z_order) 
             
             # plot the tick label
             if tick_label_format != '':
                 ax.text(
                         scale(tick[0], origin[0], scale_factor[0]), 
-                        (origin[1] + length[1] * axis_origin[1]) - tick_label_offset * length[1], 
-                        (origin[2] + length[2] * axis_origin[2]),
+                        (origin[1] + length[1] * axis_origin[0][0]) - tick_label_offset * length[1], 
+                        (origin[2] + length[2] * axis_origin[0][1]),
                         tick[1], fontsize=font_size, ha='center', va='center', zorder=z_order
                     )
-
+        '''
         # plot the axis label
         ax.text(
                 scale(origin[0] + length[0]/2, origin[0], scale_factor[0]), 
@@ -105,8 +106,9 @@ def plot_3d_axis(ax, origin, length, direction_id,
                 scale((origin[2] + length[2] * axis_origin[2]), origin[2], scale_factor[2]), 
                 axis_label, fontsize=font_size, ha='center', va='center', zorder=z_order
             )
-            
         '''
+            
+        
     '''
     elif direction_id == 1:
         # plot an y axis
