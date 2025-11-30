7934688d4e061a6785bad6a7fd47beda7eca50c7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-11-30 15:22:31 +0100

    added big arrow

--- graphics/arrow.py
+++ graphics/arrow.py
@@ -1,14 +1,11 @@
 import math
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import numpy as np
-import pandas as pd
 
 import constants.utils as const
 import list.column_labels as clab
 import graphics.utils as gr
 import calculus.geometry as geo
-
-
-
 
 
 '''
@@ -21,13 +18,17 @@ plot an arrow head, where 'arrow_shaft_start' is the start position of the shaft
 def plot_arrow_head(ax, arrow_shaft_start, arrow_shaft_end, arrow_shaft_length, dr_arrow_head, arrow_head_length, mins,
                     scale_factors, line_width, color, alpha, z_order):
     # compute arrow_head_start_scaled = arrow_shaft_end_scaled
-    arrow_shaft_start_scaled = gr.scale_list(arrow_shaft_start, mins, scale_factors)
-    arrow_shaft_end_scaled = gr.scale_list(arrow_shaft_end, mins, scale_factors)
+    arrow_shaft_start_scaled = gr.scale_list(
+        arrow_shaft_start, mins, scale_factors)
+    arrow_shaft_end_scaled = gr.scale_list(
+        arrow_shaft_end, mins, scale_factors)
 
-    dr_shaft_scaled = np.subtract(arrow_shaft_end_scaled, arrow_shaft_start_scaled)
+    dr_shaft_scaled = np.subtract(
+        arrow_shaft_end_scaled, arrow_shaft_start_scaled)
 
     norm_dr_shaft_scaled = np.sqrt(np.dot(dr_shaft_scaled, dr_shaft_scaled))
-    arrow_shaft_end_scaled = arrow_shaft_start_scaled + dr_shaft_scaled / norm_dr_shaft_scaled * arrow_shaft_length
+    arrow_shaft_end_scaled = arrow_shaft_start_scaled + \
+        dr_shaft_scaled / norm_dr_shaft_scaled * arrow_shaft_length
 
     arrow_head_start_scaled = arrow_shaft_end_scaled
     '''
@@ -37,7 +38,8 @@ def plot_arrow_head(ax, arrow_shaft_start, arrow_shaft_end, arrow_shaft_length, 
     arrow_head_end_scaled = np.add(arrow_head_start_scaled,
                                    dr_arrow_head / np.sqrt(np.dot(dr_arrow_head, dr_arrow_head)) * arrow_head_length)
 
-    arrow_head_start_end = list(zip(arrow_head_start_scaled, arrow_head_end_scaled))
+    arrow_head_start_end = list(
+        zip(arrow_head_start_scaled, arrow_head_end_scaled))
 
     ax.plot(arrow_head_start_end[0], arrow_head_start_end[1], arrow_head_start_end[2],
             color=color, linewidth=line_width, alpha=alpha, zorder=z_order)
@@ -60,36 +62,43 @@ plot an arrow
 '''
 
 
-def plot_arrow(ax, shaft_start_position, shaft_end_position, shaft_length, 
-               head_over_shaft_length, head_angle, 
+def plot_arrow(ax, shaft_start_position, shaft_end_position, shaft_length,
+               head_over_shaft_length, head_angle,
                mins,
-               scale_factors, threshold_arrow_length, line_width, 
+               scale_factors, threshold_arrow_length, line_width,
                color, alpha, z_order):
     head_length = head_over_shaft_length * shaft_length
 
     # plot the shaft
-    shaft_start_position_scaled = gr.scale_list(shaft_start_position, mins, scale_factors)
-    shaft_end_position_scaled = gr.scale_list(shaft_end_position, mins, scale_factors)
-    dr_shaft_scaled = np.subtract(shaft_end_position_scaled, shaft_start_position_scaled)
+    shaft_start_position_scaled = gr.scale_list(
+        shaft_start_position, mins, scale_factors)
+    shaft_end_position_scaled = gr.scale_list(
+        shaft_end_position, mins, scale_factors)
+    dr_shaft_scaled = np.subtract(
+        shaft_end_position_scaled, shaft_start_position_scaled)
 
     dr_shaft = np.subtract(shaft_end_position, shaft_start_position)
 
     if (np.sqrt(np.dot(dr_shaft, dr_shaft)) > threshold_arrow_length):
-        dr_shaft = dr_shaft * shaft_length / np.sqrt(np.dot(dr_shaft, dr_shaft))
+        dr_shaft = dr_shaft * shaft_length / \
+            np.sqrt(np.dot(dr_shaft, dr_shaft))
 
         shaft_end_position = np.add(shaft_start_position, dr_shaft)
         # arrow_head_start = arrow_shaft_end
 
         gr.plot_line_scaled(ax, shaft_start_position, shaft_end_position, shaft_length, mins, scale_factors, line_width,
-                         color,
-                         alpha, z_order)
+                            color,
+                            alpha, z_order)
 
-        theta_shaft = np.arccos(dr_shaft_scaled[2] / np.sqrt(np.dot(dr_shaft_scaled, dr_shaft_scaled)))
+        theta_shaft = np.arccos(
+            dr_shaft_scaled[2] / np.sqrt(np.dot(dr_shaft_scaled, dr_shaft_scaled)))
         phi_shaft = np.arctan2(dr_shaft_scaled[1], dr_shaft_scaled[0])
 
         # compute coordinates for arrow heads
-        up_head = [-head_length * np.sin(head_angle * const.deg_to_rad), 0, -head_length * np.cos(head_angle * const.deg_to_rad)]
-        down_head = [+head_length * np.sin(head_angle * const.deg_to_rad), 0, - head_length * np.cos(head_angle * const.deg_to_rad)]
+        up_head = [-head_length * np.sin(head_angle * const.deg_to_rad),
+                   0, -head_length * np.cos(head_angle * const.deg_to_rad)]
+        down_head = [+head_length * np.sin(head_angle * const.deg_to_rad),
+                     0, - head_length * np.cos(head_angle * const.deg_to_rad)]
         up_head = np.matmul(gr.R(theta_shaft, phi_shaft), up_head)
         down_head = np.matmul(gr.R(theta_shaft, phi_shaft), down_head)
 
@@ -104,32 +113,89 @@ def plot_arrow(ax, shaft_start_position, shaft_end_position, shaft_length,
 plot a 2d arrow
 
 '''
-def plot_2d_arrow(ax, shaft_start_position, shaft_end_position, shaft_length, head_over_shaft_length, head_angle, line_width, color, alpha, z_order, 
-                  threshold_arrow_length = const.default_threshold_arrow_length,
+
+
+def plot_2d_arrow(ax, shaft_start_position, shaft_end_position, shaft_length, head_over_shaft_length, head_angle, line_width, color, alpha, z_order,
+                  threshold_arrow_length=const.default_threshold_arrow_length,
                   clip_on=True):
     head_length = head_over_shaft_length * shaft_length
 
     # plot the shaft
     dr_shaft = np.subtract(shaft_end_position, shaft_start_position)
 
-    if ((np.linalg.norm(dr_shaft) > threshold_arrow_length)): 
+    if ((np.linalg.norm(dr_shaft) > threshold_arrow_length)):
 
-        dr_shaft = dr_shaft * shaft_length / np.sqrt(np.dot(dr_shaft, dr_shaft))
+        dr_shaft = dr_shaft * shaft_length / \
+            np.sqrt(np.dot(dr_shaft, dr_shaft))
 
-        ax.plot([shaft_start_position[0], shaft_start_position[0] + dr_shaft[0]], [shaft_start_position[1], shaft_start_position[1] + dr_shaft[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
+        ax.plot([shaft_start_position[0], shaft_start_position[0] + dr_shaft[0]], [shaft_start_position[1],
+                shaft_start_position[1] + dr_shaft[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
 
         theta_shaft = -np.pi / 2 + math.atan2(dr_shaft[1], dr_shaft[0])
 
         # plot the heads
         # consider heads related to a fictitious arrow pointing up
-        up_head = [-head_length * np.sin(head_angle * const.deg_to_rad), -head_length * np.cos(head_angle * const.deg_to_rad)]
-        down_head = [+head_length * np.sin(head_angle * const.deg_to_rad), - head_length * np.cos(head_angle * const.deg_to_rad)]
+        up_head = [-head_length * np.sin(head_angle * const.deg_to_rad), -
+                   head_length * np.cos(head_angle * const.deg_to_rad)]
+        down_head = [+head_length * np.sin(head_angle * const.deg_to_rad), -
+                     head_length * np.cos(head_angle * const.deg_to_rad)]
         # rotate the heads above
         up_head = np.matmul(gr.R_2d(theta_shaft), up_head)
         down_head = np.matmul(gr.R_2d(theta_shaft), down_head)
 
         # plot the heads
-        ax.plot([shaft_start_position[0] + dr_shaft[0], shaft_start_position[0] + dr_shaft[0] + up_head[0]], [shaft_start_position[1] + dr_shaft[1], shaft_start_position[1] + dr_shaft[1] + up_head[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
-        
-        ax.plot([shaft_start_position[0] + dr_shaft[0], shaft_start_position[0] + dr_shaft[0] + down_head[0]], [shaft_start_position[1] + dr_shaft[1], shaft_start_position[1] + dr_shaft[1] + down_head[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
+        ax.plot([shaft_start_position[0] + dr_shaft[0], shaft_start_position[0] + dr_shaft[0] + up_head[0]], [shaft_start_position[1] + dr_shaft[1],
+                shaft_start_position[1] + dr_shaft[1] + up_head[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
 
+        ax.plot([shaft_start_position[0] + dr_shaft[0], shaft_start_position[0] + dr_shaft[0] + down_head[0]], [shaft_start_position[1] + dr_shaft[1],
+                shaft_start_position[1] + dr_shaft[1] + down_head[1]], color=color, linewidth=line_width, alpha=alpha, zorder=z_order, clip_on=clip_on)
+
+
+'''
+plot a big arrow 
+Input values: 
+    - Mandatory: 
+        * 'ax': the 3d axis where the arrow will be plotted
+        * 'shaft_bottom_position': a list: the 3d position of the midpoint of the lower edge of the arrow shaft
+        * 'shaft_size': [length, width], the size of the arrow shaft
+        * 'head_size': [length, width], the size of the rectangle containing the arrow head
+    - Optional:
+        * 'line_width': the line width of the polygon at the edge of the arrow area
+        * 'edge_color': the color of the polygon at the edge of the arrow area
+        * 'fill_color': the color with which the interior of the arrow will be filled
+        * 'alpha': transparency of the fill color
+        * 'z_order': z_order of the arrow
+        * 'clip_on': set whether the arrow will be clipped on with respect to the axes
+'''
+
+
+def plot_big_arrow(ax, shaft_bottom_position, shaft_size, head_size,
+                   line_width=const.default_line_width,
+                   edge_color=const.default_color,
+                   fill_color=const.default_color,
+                   alpha=const.default_alpha,
+                   z_order=const.default_z_order,
+                   clip_on=False):
+
+    vertices = [([0] * 3)]
+    vertices.append([0, -shaft_size[1]/2, 0])
+    vertices.append([shaft_size[0], -shaft_size[1]/2, 0])
+    vertices.append([shaft_size[0], -head_size[1]/2, 0])
+    vertices.append([shaft_size[0] + head_size[0], 0, 0])
+    vertices.append([shaft_size[0], head_size[1]/2, 0])
+    vertices.append([shaft_size[0], shaft_size[1]/2, 0])
+    vertices.append([0, shaft_size[1]/2, 0])
+
+    for i in range(len(vertices)):
+        vertices[i] = np.add(vertices[i], shaft_bottom_position)
+
+    # Create a Poly3DCollection with the vertices
+    polygon = Poly3DCollection([vertices],
+                               linewidths=line_width,
+                               edgecolors=edge_color,
+                               facecolors=fill_color,
+                               alpha=alpha,
+                               zorder=z_order,
+                               clip_on=clip_on)
+
+    ax.add_collection3d(polygon)
