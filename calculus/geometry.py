7ab134dedcf5eba035429040d81f66ed97a40a9c
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2025-12-05 23:02:37 +0100

    moved to module

--- calculus/geometry.py
+++ calculus/geometry.py
@@ -1,19 +1,25 @@
 import numpy as np
 import pandas as pd
 
-#euclidean  norm of vector x
-def eucl_norm(x):
-    return (np.sqrt( np.dot( x, x ) ))
+# euclidean  norm of vector x
 
-#the vectors tangent to the curvilinear coordinates on the manifold : e(z)[i] = e_i_{al-izzi2020shear}
+
+def eucl_norm(x):
+    return (np.sqrt(np.dot(x, x)))
+
+# the vectors tangent to the curvilinear coordinates on the manifold : e(z)[i] = e_i_{al-izzi2020shear}
+
+
 def e(omega):
     return [[1, 0, omega[0]], [0, 1, omega[1]]]
 
-#MAKE SURE THAT THIS NORMAL IS DIRECTED OUTWARDS
-#normal(z) = \hat{n}_{al-izzi2020shear}
+# MAKE SURE THAT THIS NORMAL IS DIRECTED OUTWARDS
+# normal(z) = \hat{n}_{al-izzi2020shear}
+
+
 def normal(omega):
-    return np.cross(e(omega)[0], e(omega)[1]) /  eucl_norm(np.cross(e(omega)[0], e(omega)[1]))
-#MAKE SURE THAT THIS NORMAL IS DIRECTED OUTWARDS
+    return np.cross(e(omega)[0], e(omega)[1]) / eucl_norm(np.cross(e(omega)[0], e(omega)[1]))
+# MAKE SURE THAT THIS NORMAL IS DIRECTED OUTWARDS
 
 
 '''
@@ -26,33 +32,37 @@ Return values:
     - True if q is in the triangle, False otherwise
 
 '''
+
+
 def point_in_triangle(p1, p2, p3, q):
 
     # vectors joining the vertices
     vector_12 = np.subtract(p1, p2)
     vector_23 = np.subtract(p2, p3)
     vector_31 = np.subtract(p3, p1)
-    
+
     # midpoints between vertices
     midpoint_12 = np.add(p1, p2)/2
     midpoint_23 = np.add(p2, p3)/2
     midpoint_31 = np.add(p3, p1)/2
-    
+
     # normals to vectors joining the vertices
     normal_12 = [-vector_12[1], vector_12[0]]
     normal_23 = [-vector_23[1], vector_23[0]]
     normal_31 = [-vector_31[1], vector_31[0]]
-    
+
     # check if q is on the same side as p3 with respect to normal_12, and similarly for other edges
-    check_12 = (np.dot(np.subtract(q, midpoint_12), normal_12) * np.dot(np.subtract(p3, midpoint_12), normal_12) > 0 )
-    check_23 = (np.dot(np.subtract(q, midpoint_23), normal_23) * np.dot(np.subtract(p1, midpoint_23), normal_23) > 0 )
-    check_31 = (np.dot(np.subtract(q, midpoint_31), normal_31) * np.dot(np.subtract(p2, midpoint_31), normal_31) > 0)
-    
+    check_12 = (np.dot(np.subtract(q, midpoint_12), normal_12) *
+                np.dot(np.subtract(p3, midpoint_12), normal_12) > 0)
+    check_23 = (np.dot(np.subtract(q, midpoint_23), normal_23) *
+                np.dot(np.subtract(p1, midpoint_23), normal_23) > 0)
+    check_31 = (np.dot(np.subtract(q, midpoint_31), normal_31) *
+                np.dot(np.subtract(p2, midpoint_31), normal_31) > 0)
+
     # the point is in the triangle if all checks evaluate to True
     return check_12 and check_23 and check_31
-   
-   
-   
+
+
 '''
 check if a point lies within a mesh
 Input values: 
@@ -63,28 +73,60 @@ Return values:
     - True if 'point' is in the triangle, False otherwise
 
 '''
+
+
 def point_in_mesh(mesh_file, point):
-       
+
     # read the trinagles of the mesh
     data_triangles = pd.read_csv(mesh_file)
-    
+
     # print(data_triangles)
-    
+
     # check is True / False if 'point' is in at least one triangle in the mesh
     check = False
     # loop through the triangles of the mesh
     for index, triangle in data_triangles.iterrows():
-            
+
         # print(triangle['p_1:0'], triangle['p_1:1'])
-        
+
         if point_in_triangle(
-                                [triangle['p_1:0'], triangle['p_1:1']],
-                                [triangle['p_2:0'], triangle['p_2:1']],
-                                [triangle['p_3:0'], triangle['p_3:1']],
-                                point
-                            ):
-            
+            [triangle['p_1:0'], triangle['p_1:1']],
+            [triangle['p_2:0'], triangle['p_2:1']],
+            [triangle['p_3:0'], triangle['p_3:1']],
+            point
+        ):
+
             check = True
             break
-        
-    return check     
+
+    return check
+
+
+'''
+compute the displacement field of a line
+Input values: 
+    - 'data_X': a pandas dataframe containing the columns 'f:0', 'f:1', 'f:2' for the three coordinates of the line, and the columns ':0' for the parametric coordinate x^1, and ':1', ':2' identically zero
+'''
+
+
+def u_1d(data_X):
+
+    U_x = []
+    U_y = []
+    X_U = []
+    Y_U = []
+    for _, row in data_X.iterrows():
+
+        X_U.append(row[':0'])
+        Y_U.append(0)
+
+        U_x.append(row['f:0'] - row[':0'])
+        U_y.append(row['f:1'] - 0)
+
+    # Convert to numpy arrays
+    X_U = np.array(X_U)
+    Y_U = np.array(Y_U)
+    U_x = np.array(U_x)
+    U_y = np.array(U_y)
+
+    return X_U, Y_U, U_x, U_y
